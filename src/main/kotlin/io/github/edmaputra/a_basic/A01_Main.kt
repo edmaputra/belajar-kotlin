7b659c0cf061cc38d9b2bcd25ae31f42424094cf
Bangun Edma Saputra <bangun.edma.saputra@gmail.com>
2022-07-29 08:08:53 +0700

    Add main and comment in kotlin

---
+++ src/main/kotlin/io/github/edmaputra/a_basic/A01_Main.kt
@@ -0,0 +1,8 @@
+package io.github.edmaputra.a_basic
+
+/**
+ * 1. The first and main function to run the kotlin
+ */
+fun main() {
+    println("hello kotlin")
+}
---
+++ src/main/kotlin/io/github/edmaputra/a_basic/A02_Comment.kt
@@ -0,0 +1,13 @@
+package io.github.edmaputra.a_basic
+
+// single line comment
+
+/*
+Multi
+line
+comment
+ */
+
+/**
+ * Comment for documentation
+ */
